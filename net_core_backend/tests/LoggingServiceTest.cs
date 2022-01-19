f0df8024b47b575cf10066a875eefe63351a1ac6
Vitali Bestolkau <bestolkovv@gmail.com>
2022-01-20 00:51:18 +0100

    Did the tests but the Logging tests don't work properly

---
+++ net_core_backend/tests/LoggingServiceTest.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using net_core_backend.Models;
+using net_core_backend.Services;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace tests
+{
+    public class LoggingServiceTest
+    {
+        private readonly IDbContextFactory<OneBlinqDBContext> testContextFactory;
+        private LoggingService sut;
+        public LoggingServiceTest()
+        {
+            // Mocking database
+            testContextFactory = new TestContextFactory();
+
+            // Mocking IMailingService 
+            var mockMailingService = new Mock<IMailingService>();
+
+            sut = new LoggingService(testContextFactory, mockMailingService.Object);
+        }
+
+        [Fact]
+        public async Task AddActivationLogTest()
+        {
+            String licenseKey = "SuperSecretLicenseKey";
+
+            var product = new Products()
+            {
+                ProductName = "Cool name",
+            };
+
+            var user = new Users()
+            {
+                Email = "coll@gmail.com"
+            };
+
+            var license = new Licenses()
+            {
+                LicenseKey = licenseKey,
+                Product = product,
+                User = user
+            };
+
+            var db = testContextFactory.CreateDbContext();
+            await db.Products.AddAsync(product);
+            await db.Users.AddAsync(user);
+            await db.SaveChangesAsync();
+            await db.Licenses.AddAsync(license);
+            await db.SaveChangesAsync();
+
+            bool successful = true;
+            String ExternalUniqueUserId = "SuperUniqueUserId";
+            String platformName = "SuperAwesomePlatformName";
+            String message = "The user managed to use their license super successfully";
+
+            await sut.AddActivationLog(licenseKey, successful, ExternalUniqueUserId, platformName, message);
+
+            var dbLicense = await db.Licenses.FirstOrDefaultAsync(l => l.LicenseKey == licenseKey);
+
+            var activationLog = await db.ActivationLogs.FirstOrDefaultAsync(al => al.Message == message);
+
+            activationLog.ShouldNotBeNull();
+            activationLog.Message.ShouldBe(message);
+        }
+
+        [Fact]
+        public async Task RemoveUniqueUserIdLogsTest()
+        {
+            String externalUserServiceId = "someId";
+
+            var uniqueUser = new UniqueUsers()
+            {
+                ExternalUserServiceId = externalUserServiceId
+            };
+
+            String licenseKey = "somekey";
+
+            var license = new Licenses()
+            {
+                LicenseKey = licenseKey,
+            };
+
+            var actLog1 = new ActivationLogs()
+            {
+                UniqueUser = uniqueUser,
+                License = license,
+                Message = "Some cool message1"
+            };
+
+            var actLog2 = new ActivationLogs()
+            {
+                UniqueUser = uniqueUser,
+                License = license,
+                Message = "Some cool message2"
+            };
+
+            var db = testContextFactory.CreateDbContext();
+
+            await db.UniqueUsers.AddAsync(uniqueUser);
+            await db.Licenses.AddAsync(license);
+            await db.SaveChangesAsync();
+            await db.ActivationLogs.AddAsync(actLog1);
+            await db.ActivationLogs.AddAsync(actLog2);
+            await db.SaveChangesAsync();
+
+            var dbUniqueUser = await db.UniqueUsers.Include(us => us.ActivationLogs).FirstOrDefaultAsync(us => us.ExternalUserServiceId == uniqueUser.ExternalUserServiceId);
+            var dbLicense = await db.Licenses.FirstOrDefaultAsync(l => l.LicenseKey == licenseKey);
+
+            await sut.RemoveUniqueUserIdLogs(dbUniqueUser.Id, dbLicense.Id);
+
+            db.Entry(dbUniqueUser).Reload();
+            db.Update(dbUniqueUser);
+
+            var dbActLog = await db.ActivationLogs.FirstOrDefaultAsync(al => al.Message == actLog1.Message);
+            var dbUpdateUniqueUser = await db.UniqueUsers.
+                Include(us => us.ActivationLogs).
+                FirstOrDefaultAsync(us => us.ExternalUserServiceId == externalUserServiceId);
+
+            dbActLog.ShouldBeNull();
+            dbUpdateUniqueUser.ActivationLogs.ShouldNotContain(actLog1);
+        }
+    }
+}
